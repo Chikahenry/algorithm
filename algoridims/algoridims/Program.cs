bc8b1437487b4e0ce9a62fdfd389ecdcf8a5190d
Chikahenry <49327206+Chikahenry@users.noreply.github.com>
2019-09-29 16:28:44 +0100

    Add files via upload

---
+++ algoridims/algoridims/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoridims 
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> a = new List<int>() { 2, 3, 4 };
+            List<int> b = new List<int>() { 4, 3, 5 };
+            for (int i = 0; i < 3; i++)
+            {
+                var va = a[i] * a[i + 1] * a[i + 2];
+                var vb = b[i] * b[i + 1] * b[i + 2];
+
+                var vol = va - vb;
+                if (vol < 0)
+                {
+                    Console.WriteLine(vol * -1) ;
+                }
+                else
+                {
+                    Console.WriteLine(vol);
+                }
+
+            }
+
+        }
+        
+    }
+    
+}
